79af1e1dd2a81eb53f64500dc5d240e07157aa04
YehyunLee <glad7cu@gmail.com>
2024-02-17 02:57:11 -0500

    FIx

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,9 +16,6 @@ const config = {
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
 
-    "@typescript-eslint/no-unused-vars": "off",  // added
-    "react/no-string-refs": "off",  // added
-
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
@@ -26,7 +23,7 @@ const config = {
         fixStyle: "inline-type-imports",
       },
     ],
-    // "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/require-await": "off",
     "@typescript-eslint/no-misused-promises": [
       "error",
