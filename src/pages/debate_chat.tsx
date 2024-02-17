0d06acc57cacc37aeca64a14e734801f98e031a4
YehyunLee <glad7cu@gmail.com>
2024-02-17 17:15:02 -0500

    Fix

--- src/pages/debate_chat.tsx
+++ src/pages/debate_chat.tsx
@@ -1,5 +1,36 @@
-// const debate_chat = () => {
+import VoiceToText from "~/components/VoiceToText";
 
 
+const debate_chat = () => {
+// delete
 
-// }
+    return (
+        <>
+        <div className="bubble left">Bro ipsum dolor sit amet gaper backside single track, manny Bike epic clipless. Schraeder drop gondy, rail fatty slash gear jammer steeps</div>
+        <div className="bubble right">Ok, Thank you</div>
+        <div className="bubble left"> ut labore et dolore magna </div>
+        <div className="bubble right">Ok, Thank you</div>
+        <div className="bubble right">Ok, Thank you</div>
+        <div className="bubble right">Ok, Thank you</div>
+
+        <div className="bubble right">Ok, Thank you</div>
+
+        <div className="bubble right">Ok, Thank you</div>
+
+
+        <div className="bubble right">Ok, Thank you</div>
+
+        <div className="bubble right">Ok, Thank you</div>
+
+        <div className="bubble right">Ok, Thank you</div>
+
+
+        <VoiceToText />
+
+        </>
+    );
+
+
+}
+
+export default debate_chat;
