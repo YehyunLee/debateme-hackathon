41320c528f0a987687296b9bec2737d294c86c0c
YehyunLee <glad7cu@gmail.com>
2024-02-17 00:50:51 -0500

    Renamed

--- src/pages/speak.tsx
+++ src/pages/speak.tsx
@@ -1,12 +1,13 @@
 export default function TextToSpeech() {
   const speak = (text) => {
     const utterance = new SpeechSynthesisUtterance(text);
+    utterance.lang = 'en-US'; // Set the language to English
     window.speechSynthesis.speak(utterance);
   };
 
   return (
     <div>
-      <button onClick={() => speak('감사!')}>Speak</button>
+      <button onClick={() => speak('HI!')}>Speak</button>
     </div>
   );
 }
