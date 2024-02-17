6c704225313c6493d28cb938e92f6401f93d4003
logicctdt <logicirz@gmail.com>
2024-02-17 08:44:14 -0500

    fixed error in index

--- src/components/debateLeaderboard.tsx
+++ src/components/debateLeaderboard.tsx
@@ -1,28 +1,32 @@
-// type UserTuple = [string, number];
+import React from 'react';
 
-// const debateLeaderboard = (users: UserTuple[]): JSX.Element => {
-//     // Mapping each tuple to a table row
-//     const rows: React.JSX[] = users.map((user: UserTuple, index: number) => (
-//         <tr key={index}>
-//             <td>{user[0]}</td> {/* Username */}
-//             <td>{user[1]}</td> {/* Integer value */}
-//         </tr>
-//     ));
+interface Props {
+    data: [string, number][];
+}
 
-//     // Returning the table structure
-//     return (
-//         <table>
-//             <thead>
-//                 <tr>
-//                     <th>Username</th>
-//                     <th>Score</th>
-//                 </tr>
-//             </thead>
-//             <tbody>
-//                 {rows} {/* Inserting the rows */}
-//             </tbody>
-//         </table>
-//     );
-// }
+const debateLeaderboard: React.FC<Props> = ({ data }) => {
+    return (
+        <>
+        <h1 className='text-1E635F text-center pt-[2rem] mb-[1rem] text-3xl font-bold' > Leaderboard </h1>
+        <table className='text-center mr-auto ml-auto w-[50vw] table-fixed border border-seperate border-slate-500 text-xl rounded-xl '>
+            <thead>
+                <tr>
+                    <th className='border-b-2 border-slate-500 '>Username</th>
+                    <th className='border-b-2 border-slate-500'>Score</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data.map(([username, score]: [string, number], index: number) => (
+                    <tr key={index}>
+                        <td >{username}</td>
+                        <td>{score}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+        </>
+    );
+}
 
-// export default debateLeaderboard;
+
+export default debateLeaderboard;
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,10 +1,17 @@
+import { headers } from "next/headers";
 import React from "react";
 
 const Nav: React.FC = () => {
   return (
-    <nav className="flex items-center justify-between bg-white px-6 py-4">
-      <p className="text-1E635F text-2xl font-bold">Debateme</p>
+    <header
+  className="sticky top-0 z-10 pt-6 pb-5 backdrop-filter backdrop-blur
+                   dark:bg-primary dark:text-white">
+    <nav className="flex items-center justify-between h-16 font-semibold text-sm
+       after:absolute after:inset-x-0 after:w-full after:h-12 after:shadow-hr after:z-[-1]">
+      <p className="text-1E635F text-2xl font-bold ml-[1rem]"> Debate Me</p>
+      
     </nav>
+    </header>
   );
 };
 
