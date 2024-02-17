e405eb402b6cc96f5cb09a9ae7b170d801b22a5f
YehyunLee <glad7cu@gmail.com>
2024-02-17 02:55:29 -0500

    Fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -26,7 +26,7 @@ const config = {
         fixStyle: "inline-type-imports",
       },
     ],
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    // "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/require-await": "off",
     "@typescript-eslint/no-misused-promises": [
       "error",
