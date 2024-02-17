8b54f5906a05a56dbadf6e5e0988c7a14df9de02
YehyunLee <glad7cu@gmail.com>
2024-02-17 02:40:54 -0500

    Speak fix

--- src/pages/speak.tsx
+++ src/pages/speak.tsx
@@ -1,5 +1,5 @@
 export default function TextToSpeech() {
-  const speak = (text) => {
+  const speak = (text: string) => { // Add type annotation to 'text' parameter
     const utterance = new SpeechSynthesisUtterance(text);
     utterance.lang = 'en-US'; // Set the language to English
     window.speechSynthesis.speak(utterance);
