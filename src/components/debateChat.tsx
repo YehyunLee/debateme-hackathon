88cda5e7d3b9fe6130fb5cc46956fae6a9a37fd2
YehyunLee <glad7cu@gmail.com>
2024-02-17 16:51:03 -0500

    Fix

--- src/components/debateChat.tsx
+++ src/components/debateChat.tsx
@@ -6,23 +6,23 @@ const DebateChat = () => {
 
     return (
         <>
-        <div class="bubble left">Bro ipsum dolor sit amet gaper backside single track, manny Bike epic clipless. Schraeder drop gondy, rail fatty slash gear jammer steeps</div>
-        <div class="bubble right">Ok, Thank you</div>
-        <div class="bubble left"> ut labore et dolore magna </div>
-        <div class="bubble right">Ok, Thank you</div>
-        <div class="bubble right">Ok, Thank you</div>
-        <div class="bubble right">Ok, Thank you</div>
+        <div className="bubble left">Bro ipsum dolor sit amet gaper backside single track, manny Bike epic clipless. Schraeder drop gondy, rail fatty slash gear jammer steeps</div>
+        <div className="bubble right">Ok, Thank you</div>
+        <div className="bubble left"> ut labore et dolore magna </div>
+        <div className="bubble right">Ok, Thank you</div>
+        <div className="bubble right">Ok, Thank you</div>
+        <div className="bubble right">Ok, Thank you</div>
 
-        <div class="bubble right">Ok, Thank you</div>
+        <div className="bubble right">Ok, Thank you</div>
 
-        <div class="bubble right">Ok, Thank you</div>
+        <div className="bubble right">Ok, Thank you</div>
 
 
-        <div class="bubble right">Ok, Thank you</div>
+        <div className="bubble right">Ok, Thank you</div>
 
-        <div class="bubble right">Ok, Thank you</div>
+        <div className="bubble right">Ok, Thank you</div>
 
-        <div class="bubble right">Ok, Thank you</div>
+        <div className="bubble right">Ok, Thank you</div>
 
 
         <VoiceToText />
