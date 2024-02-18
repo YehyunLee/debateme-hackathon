10b999aa3cc9b3d25032fb1c470e8f90c379a98e
YehyunLee <glad7cu@gmail.com>
2024-02-17 23:41:16 -0500

    Live

--- src/pages/play.tsx
+++ src/pages/play.tsx
@@ -34,9 +34,9 @@ export default function Home() {
     isLoading: true,
   });
 
-  const [playMode, setPlayMode] = useState<"normal" | "crazy">("normal"); // Initialize state for play mode
+  const [playMode, setPlayMode] = useState<"normal" | "crazy" | "init">("init"); // Initialize state for play mode
 
-  const handlePlayMode = (mode: "normal" | "crazy") => {
+  const handlePlayMode = (mode: "normal" | "crazy" | "init") => {
     setPlayMode(mode);
     setDebateChatEnabled(true);
   };
@@ -71,19 +71,18 @@ export default function Home() {
     fetchUserData();
   }, [sessionData]);
 
-
-
-
   const [interests, setInterests] = useState(""); // Initialize state for interests
 
   const createUser = async () => {
     try {
       if (sessionData?.user?.name) {
         // Split interests by comma and trim each value
-        const interestsArray = interests.split(",").map(interest => interest.trim());
+        const interestsArray = interests
+          .split(",")
+          .map((interest) => interest.trim());
 
-        console.log(interestsArray, sessionData.user.id, sessionData.user.name)
-    
+        console.log(interestsArray, sessionData.user.id, sessionData.user.name);
+
         // Make a POST request to the Flask route
         const response = await axios.post(
           "https://web-production-a23d.up.railway.app/create_user",
@@ -94,21 +93,20 @@ export default function Home() {
           },
           {
             headers: {
-              "Content-Type": "application/json"
-            }
-          }
+              "Content-Type": "application/json",
+            },
+          },
         );
-    
+
         // Handle the response as needed
         console.log(response.data); // Should log "user created" if successful
       }
     } catch (error) {
       console.error("Error creating user:", error);
     }
+    // refresh
+    router.reload();
   };
-
-
-
 
   useEffect(() => {
     if (status === "loading") return; // Still loading, don't do anything yet
@@ -129,77 +127,75 @@ export default function Home() {
       {!debateChatEnabled && (
         <div className="flex flex-col md:flex-row">
           <div className="w-full p-4 md:w-1/2">
-
             {!userData.isLoading && (
-            <div className="rounded-3xl bg-DAF2F1 p-4 shadow-2xl mb-4">
-              <h2 className="mb-4 text-2xl font-bold">Profile</h2>
-              {userData.isLoading ? (
-                <p>Loading...</p>
-              ) : (
-                <div className="mb-4 rounded-lg bg-white p-4">
-                  <p className="mb-2 text-black">
-                    <span className="font-bold">Current DPA:</span>{" "}
-                    {userData.dpa}
-                  </p>
-                  <p className="mb-2 text-black">
-                    <span className="font-bold">Interested Topics:</span>{" "}
-                    {userData.interests}
-                  </p>
-                </div>
-              )}
-            </div>
-          )}
+              <div className="mb-4 rounded-3xl bg-DAF2F1 p-4 shadow-2xl">
+                <h2 className="mb-4 text-2xl font-bold">Profile</h2>
+                {userData.isLoading ? (
+                  <p>Loading...</p>
+                ) : (
+                  <div className="mb-4 rounded-lg bg-white p-4">
+                    <p className="mb-2 text-black">
+                      <span className="font-bold">Current DPA:</span>{" "}
+                      {userData.dpa}
+                    </p>
+                    <p className="mb-2 text-black">
+                      <span className="font-bold">Interested Topics:</span>{" "}
+                      {userData.interests}
+                    </p>
+                  </div>
+                )}
+              </div>
+            )}
 
             <div className="rounded-3xl bg-DAF2F1 p-4 shadow-2xl">
-        <h2 className="mb-4 text-2xl font-bold">Create Account</h2>
-        <div className="mb-4 rounded-lg bg-white p-4">
-          <label htmlFor="interests" className="block text-gray-700">
-            Type interests (e.g., "AI, Politics, Sports"):
-          </label>
-          <input
-            type="text"
-            id="interests"
-            name="interests"
-            value={interests} // Bind the input value to the state
-            onChange={(e) => setInterests(e.target.value)} // Update the state when the input changes
-            className="mt-1 block w-full rounded-lg border-gray-300 p-2 focus:border-indigo-500 focus:outline-none"
-          />
-        </div>
-        <button
-          onClick={createUser} // Call createUser when the button is clicked
-          className="rounded-lg bg-red-500 text-white px-4 py-2 hover:bg-blue-600"
-        >
-          Create Account / Reset Elo
-        </button>
-      </div>
-
-      {!userData.isLoading && (
-
-            <div className="mt-4 rounded-3xl bg-DAF2F1 p-4 shadow-2xl">
-              <h2 className="mb-4 text-2xl font-bold">Play</h2>
+              <h2 className="mb-4 text-2xl font-bold">Create Account</h2>
               <div className="mb-4 rounded-lg bg-white p-4">
-                <p className="mb-2 text-1E635F">
-                  Normal - debate based on topics of your interests
-                </p>
-                <p className="text-red-600">
-                  Crazy - debate random topics and assigned arguments. Increased
-                  bonus and penalties.
-                </p>
+                <label htmlFor="interests" className="block text-gray-700">
+                  Type interests (e.g., "AI, Politics, Sports"):
+                </label>
+                <input
+                  type="text"
+                  id="interests"
+                  name="interests"
+                  value={interests} // Bind the input value to the state
+                  onChange={(e) => setInterests(e.target.value)} // Update the state when the input changes
+                  className="mt-1 block w-full rounded-lg border-gray-300 p-2 focus:border-indigo-500 focus:outline-none"
+                />
               </div>
               <button
-                className={`mr-2 rounded-lg bg-1E635F px-4 py-2 text-white ${playMode === "normal" ? "opacity-100" : "opacity-50"}`}
-                onClick={() => handlePlayMode("normal")}
+                onClick={createUser} // Call createUser when the button is clicked
+                className="rounded-lg bg-red-500 px-4 py-2 text-white hover:bg-blue-600"
               >
-                NORMAL
-              </button>
-              <button
-                className={`rounded-lg bg-red-600 px-4 py-2 text-white ${playMode === "crazy" ? "opacity-100" : "opacity-50"}`}
-                onClick={() => handlePlayMode("crazy")}
-              >
-                CRAZY
+                Create/Reset Account
               </button>
             </div>
-          )}
+
+            {!userData.isLoading && (
+              <div className="mt-4 rounded-3xl bg-DAF2F1 p-4 shadow-2xl">
+                <h2 className="mb-4 text-2xl font-bold">Play</h2>
+                <div className="mb-4 rounded-lg bg-white p-4">
+                  <p className="mb-2 text-1E635F">
+                    Normal - debate based on topics of your interests
+                  </p>
+                  <p className="text-red-600">
+                    Crazy - debate random topics and assigned arguments.
+                    Increased bonus and penalties.
+                  </p>
+                </div>
+                <button
+                  className={`mr-2 rounded-lg bg-1E635F px-4 py-2 text-white ${playMode === "normal" ? "opacity-100" : "opacity-50"} hover:opacity-100`}
+                  onClick={() => handlePlayMode("normal")}
+                >
+                  NORMAL
+                </button>
+                <button
+                  className={`rounded-lg bg-red-600 px-4 py-2 text-white ${playMode === "crazy" ? "opacity-100" : "opacity-50"} hover:opacity-100`}
+                  onClick={() => handlePlayMode("crazy")}
+                >
+                  CRAZY
+                </button>
+              </div>
+            )}
           </div>
           <div className="w-full p-4 md:w-1/2">
             <div className="rounded-3xl bg-DAF2F1 p-4 shadow-2xl">
